ca4d6b5bbf80a751d65a37d028e271b026ee9581
helonglh <helong_lh@163.com>
2017-02-07 09:20:21 +0800

    Initial Commit

---
+++ DemoHLScanView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoHLScanView
+//
+//  Created by Helong on 17/2/7.
+//  Copyright © 2017年 Helong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DemoHLScanView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoHLScanView
+//
+//  Created by Helong on 17/2/7.
+//  Copyright © 2017年 Helong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
